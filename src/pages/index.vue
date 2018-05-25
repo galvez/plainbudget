bb43b413a8efc59ad58fd3c271abf39b4ea157cf
Jonas Galvez <jonasgalvez@gmail.com>
2018-05-25 05:59:24 -0300

    Add findPreviousLineBreak()

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,7 +2,9 @@
   <textarea ref="ta" class="ta"
     v-model="text"
     @click="handler"
-    @keyup.native="handler">
+    @keydown.native="handler"
+    @keydown.up="handler"
+    @keydown.down="handler">
   hello world
   foo bar
   new line
@@ -22,23 +24,38 @@ export default {
     }
   },
   methods: {
-    findNextCaretPosition (currentPos) {
+    findNextCaretPosition (lineStart) {
+      const firstSlice = this.text.slice(0, lineStart)
+      const secondSlice = this.text.slice(lineStart)
+      const nextNewLine = secondSlice.indexOf('\n')
+      const finalSlice = nextNewLine !== -1
+        ? secondSlice.slice(0, nextNewLine)
+        : secondSlice
+      const nextPos = finalSlice.search(/\d\s+/)
+      if (nextPos !== -1) {
+        return (firstSlice.length + nextPos) + 1
+      } else {
+        return (finalSlice.length + secondSlice.length) + 1
+      }
+    },
+    findPreviousLineBreak (currentPos) {
       if (currentPos === -1) {
         return
       }
       const len = this.text.length - 1
-      for (let p = currentPos; p < len; p++) {
-        console.log('this.text[p]', p, '-', this.text[p])
+      for (let p = currentPos; p > 0; p--) {
+        // console.log('this.text[p]', p, '-', this.text[p])
         if (this.text[p].match(/\n/)) {
-          return p
+          return p + 1
         }
       }
-      return len + 1
+      return 0
     },
     handler () {
       const currentPos = this.$refs.ta.selectionStart
       console.log('currentPos', currentPos)
-      const newPos = this.findNextCaretPosition(currentPos)
+      const prevLineBreak = this.findPreviousLineBreak(currentPos)
+      const newPos = this.findNextCaretPosition(prevLineBreak)
       console.log('newPos', newPos)
       this.$nextTick(() => {
         this.$refs.ta.setSelectionRange(newPos, newPos)
