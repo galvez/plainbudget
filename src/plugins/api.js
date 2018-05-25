f0aa221529696dc42a05b229ebb37aae71246b63
Jonas Galvez <jonasgalvez@gmail.com>
2018-05-25 05:38:22 -0300

    Remove unused plugins

--- src/plugins/api.js
+++
@@ -1,9 +0,0 @@
-import _, { Store } from 'vuex' // eslint-disable-line
-
-Store.prototype.hasActionHandler = function (actionHandler) {
-  return actionHandler in this._actions
-}
-
-Store.prototype.api = function (apiMethod, payload) {
-  return this.dispatch('api', { apiMethod, payload }, { root: true })
-}
