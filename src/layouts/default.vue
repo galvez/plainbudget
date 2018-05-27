22f45cc8d926cc9fcb41399436a29c9d9444fcd9
Jonas Galvez <jonasgalvez@gmail.com>
2018-05-27 07:21:23 -0300

    Moved fonts to assets

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -23,28 +23,28 @@ export default {
   font-family: 'Fira Mono';
   font-style: normal;
   font-weight: 400;
-  src: local('Fira Mono Regular'), local('FiraMono-Regular'), url(/Fira-Mono-Latin-Ext.woff2) format('woff2');
+  src: local('Fira Mono Regular'), local('FiraMono-Regular'), url(~/assets/Fira-Mono-Latin-Ext.woff2) format('woff2');
   unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
 }
 @font-face {
   font-family: 'Fira Mono';
   font-style: normal;
   font-weight: 400;
-  src: local('Fira Mono Regular'), local('FiraMono-Regular'), url(/Fira-Mono-Latin.woff2) format('woff2');
+  src: local('Fira Mono Regular'), local('FiraMono-Regular'), url(~/assets/Fira-Mono-Latin.woff2) format('woff2');
   unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
 }
 @font-face {
   font-family: 'Sorts Mill Goudy';
   font-style: normal;
   font-weight: 400;
-  src: local('Sorts Mill Goudy Regular'), local('SortsMillGoudy-Regular'), url(/SortsMillGoudy-Regular-Latin-Ext.woff2) format('woff2');
+  src: local('Sorts Mill Goudy Regular'), local('SortsMillGoudy-Regular'), url(~/assets/SortsMillGoudy-Regular-Latin-Ext.woff2) format('woff2');
   unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
 }
 @font-face {
   font-family: 'Sorts Mill Goudy';
   font-style: normal;
   font-weight: 400;
-  src: local('Sorts Mill Goudy Regular'), local('SortsMillGoudy-Regular'), url(/SortsMillGoudy-Regular-Latin.woff2) format('woff2');
+  src: local('Sorts Mill Goudy Regular'), local('SortsMillGoudy-Regular'), url(~/assets/SortsMillGoudy-Regular-Latin.woff2) format('woff2');
   unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
 }
 </style>
