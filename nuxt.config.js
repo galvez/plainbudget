6dd1c8605792c8a81d565e0c74dff15518161ed6
Jonas Galvez <jonasgalvez@gmail.com>
2018-05-27 07:22:30 -0300

    Prep nuxt generate

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,16 +1,6 @@
-require('dotenv').config()
 
 module.exports = {
-  env: {
-    port: 4333,
-    baseUrl: process.env.BASE_URL || 'http://localhost:4333'
-  },
-  router: {
-    scrollBehavior (to, from, savedPosition) {
-      return { x: 0, y: 0 }
-    }
-  },
-  srcDir: 'src/',
+  srcDir: 'src',
   head: {
     title: 'Plainbudget',
     meta: [
@@ -19,35 +9,37 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'Sales CMS.' }
     ]
   },
-  plugins: [
-    { src: '~/plugins/elementui', ssr: true }
-  ],
-  css: [
-    'font-awesome/css/font-awesome.min.css',
-    'element-theme-default/lib/index.css'
-  ],
+  generate: {
+    routes: [
+      '/'
+    ]
+  },
+  router: {
+    base: '/'
+  },
   build: {
-    vendor: ['element-ui'],
-    loaders: [
-      {
-        test: /\.(png|jpe?g|gif|svg)$/,
-        loader: 'url-loader',
-        query: {
-          limit: 1000, // 1KO
-          name: 'img/[name].[hash:7].[ext]'
-        }
-      },
-      {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
-        query: {
-          limit: 1000, // 1 KO
-          name: 'fonts/[name].[hash:7].[ext]'
-        }
-      }
-    ],
-    extend (config, ctx) {
-      if (ctx.isClient) {
+    // loaders: [
+    //   {
+    //     test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+    //     loader: 'url-loader',
+    //     query: {
+    //       limit: 1000, // 1 KO
+    //       name: 'fonts/[name].[hash:7].[ext]'
+    //     }
+    //   },
+    //   {
+    //     test: /\.(png|jpe?g|gif|svg)$/,
+    //     loader: 'url-loader',
+    //     query: {
+    //       limit: 1000, // 1KO
+    //       name: 'img/[name].[hash:7].[ext]'
+    //     }
+    //   }
+    // ],
+    extend (config, { isDev }) {
+      if (!isDev) {
+        config.output.publicPath = './_nuxt/'
+      } else {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
